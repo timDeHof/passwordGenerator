a68ab57bbce72bd896a5a0c7f7c452008492a053
timDeHof <this.is.tim.dehof@gmail.com>
2023-10-19 23:57:34 -0400

    chore(app.jsx): removed unused useeffect and React

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 
 const characters =
   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()_-+={}[],|:;<>./?".split(
